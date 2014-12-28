6d18754e9ecc7c05c5517200842e40e3ab6e1242
John Gliksberg <jg.trosh@gmail.com>
2014-12-28 14:23:40 +0100

    agrandit la zone de travail de noac.c (solution un peu pourrie ...)

--- noac.c
+++ noac.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <unac.h>
 
+#define LINESIZE 4096
+// J'ARRIVE PAS A FAIRE MARCHER DES GROSSES LIGNES
+// AVEC > 0 ACCENTS ET UN PETIT LINESIZE MAIS AU
+// MOINS LA CA MARCHE DONC ESPERONS QUE Y'A PAS DE
+// TROP GROSSE LIGNE OU QUE QUELQU'UN D'INTELLIGENT
+// FIXE CA :)
+
 int main(int argc, char* argv[]) {
 	char *out = NULL, *fnout, *line;
 	size_t out_length = 0;
@@ -16,30 +23,34 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	for (i=1; i<argc; i++) {
-		if ((fin=fopen(argv[i], "r"))==NULL) {
+		fin = fopen(argv[i], "r");
+		if (fin == NULL) {
 			fprintf(stderr, "mauvais entree %s\n", argv[i]);
 			return 1;
 		}
 		if ( unac_string("UTF-8", argv[i], strlen(argv[i]),
-						 &out, &out_length) ) {
+		                 &out, &out_length) ) {
 			printf("unac_string error on %s\n", argv[i]);
 			return 1;
 		}
-		fnout = malloc(50);
+		fnout = malloc(100);
 		sprintf(fnout, "noac%s", out);
-		printf("fnout: %s\n", fnout);
+		printf("output filename: %s\n", fnout);
 		free(out);
 		out = NULL;
 		out_length = 0;
-		if ((fout=fopen(fnout, "w"))==NULL) {
+		fout = fopen(fnout, "w");
+		if (fout == NULL) {
 			fprintf(stderr, "mauvaise sortie %s\n", fnout);
 			return 1;
 		}
-		line = malloc(200);
-		while (fgets(line, 200, fin)) {
+		free(fnout);
+		line = malloc(LINESIZE);
+		while (fgets(line, LINESIZE, fin)) {
 			if ( unac_string("UTF-8", line, strlen(line),
 			                 &out, &out_length) ) {
 				printf("unac_string error on %s\n", line);
+				printf("length: %d\n", strlen(line));
 				return 1;
 			}
 			fputs(out, fout);
@@ -47,6 +58,9 @@ int main(int argc, char* argv[]) {
 			out = NULL;
 			out_length = 0;
 		}
+		fclose(fout);
+		free(line);
+		fclose(fin);
 	}
 	return 0;
 }
