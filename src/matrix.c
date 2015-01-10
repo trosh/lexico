a30b9946aa4ec14e52b48da1f17721a90c091149
Yann Kergutuil <yann.kergutuil@gmail.com>
2015-01-10 14:22:03 +0100

    ajout matrix

--- src/matrix.c
+++ src/matrix.c
@@ -1,3 +1,12 @@
 #include "matrix.h"
 
 
+void malloc_matrix(matrix* m,int taille) {
+	int i;
+	m->mat = malloc(taille*sizeof(float*));
+	m->contenu = malloc(taille*taille*sizeof(float));
+	for(i=0;i<taille;i++)
+		m->mat[i]=&(m->contennu[taille*i]);
+	for(i=0;i<taille*taille;i++)
+		m->contenu[i]=0.;
+}
