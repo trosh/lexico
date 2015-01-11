f17b8ec4620691b4dbfb9481c85ec1d07754e4d7
Yann Kergutuil <yann.kergutuil@gmail.com>
2015-01-11 13:23:43 +0100

    matrix.c

--- include/matrix.h
+++ include/matrix.h
@@ -18,13 +18,12 @@ typedef struct {
 void malloc_matrix(matrix* mat, int taille);
 void init_matrix_word(matrix* m, dictionnaire* dico);
 void dist_polia();
-<<<<<<< HEAD
+//<<<<<<< HEAD
 float setDist(int id_s1,int  id_s2, matrix dist_mat);
-float setDistSym(int s1,int  s2, matrix dist_mat)
-=======
-float setDist(int, int, matrix);
-void setDistSym();
->>>>>>> e4d2e6d2cbb4eed763ace51ece3ab90793159fc6
+float setDistSym(int s1,int  s2, matrix dist_mat);
+//=======
+float setDist(int s1,int  s2, matrix dist_mat);
+//>>>>>>> e4d2e6d2cbb4eed763ace51ece3ab90793159fc6
 void dist_polia();
 
 #endif //MATRIX_H
